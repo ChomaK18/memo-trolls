1fc9521dad3a6572d3db3db58a0add3d4921c3da
ChomaK18 <42979144+ChomaK18@users.noreply.github.com>
2022-08-20 21:25:29 +0200

    Update Card.js
    comments have been removed

--- scr/components/Card.js
+++ scr/components/Card.js
@@ -16,41 +16,17 @@ const Card = ({ photoId, id, faceUpCard }) => {
   const srcImgBack = `/images/back.jpg`;
 
   useEffect(() => {
-    console.log("FaceUpCards: " + JSON.stringify(faceUpCards));
     faceUpCards.includes(photoId) || isInPair()
       ? setIsHidden(false)
       : setIsHidden(true);
   }, [faceUpCards, pair]);
 
-  // useEffect(() => {
-  //   //
-  //   //
-  //   // pair to jest array of arrays więc nie pobierze ID
-  //   //
-  //   //
-  //   console.log("Odkryte ? Id: " + id + " : " + isInPair());
-  //   isInPair() ? setIsHidden(false) : setIsHidden(true);
-  // }, [pair]);
-
   const isInPair = () => {
     const currentCard = { id: id, photoId: photoId };
-    // const isFirstCard = pair.length == 1 ? pair[0][0] == id : false;
-    // const isSecondCard = pair.length == 2 ? pair[1][0] == id : false;
     const firstCard = pair.length >= 1 ? pair[0].id : "BRAK";
     const secondCard = pair.length == 2 ? pair[1].id : "BRAK";
-    console.log("1st: " + firstCard + " ||| " + "2nd: " + secondCard);
-    console.log("CurrentCard: " + pair.some((c) => c.id === id));
-    // return isFirstCard || isSecondCard;
     return pair.some((c) => c.id === id);
   };
-
-  // useEffect(() => {
-  //   console.log("Pair: " + JSON.stringify(pair));
-  //   console.log("FirstCard: " + pairValues.current.firstCard);
-  //   pairValues.current.firstCard === id
-  //     ? setIsHidden(false)
-  //     : setIsHidden(true);
-  // }, [pairValues.current.firstCard]);
 
   return (
     <div className="p-0 m-1 card">
